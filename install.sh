6b195a5b75940e26dd38ffe14006d4f38be5c5fe
kamino <kamino@imea.me>
2022-03-01 14:08:45 +0800

    upload

---
+++ install.sh
@@ -0,0 +1,2 @@
+sudo apt update
+sudo apt install -y build-essential m4 crossbuild-essential-armhf crossbuild-essential-arm64 openjdk-17-jdk libncurses5-dev libssl-dev zlib1g-dev gettext texinfo pkgconf flex mkbootimg
